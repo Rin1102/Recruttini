c856356659fd2d661ec72bedee1548d3fa1456c4
Rin1102 <sirine.bouchnak@polytechnicien.tn>
2026-04-25 14:37:55 +0100

    remove wrong files

---
+++ Downloads/Recruttini-mariem/plateforme_recrutement/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Auth
+    path('inscription/', views.inscription, name='inscription'),
+    path('connexion/', views.connexion, name='connexion'),
+    path('deconnexion/', views.deconnexion, name='deconnexion'),
+    path('profil/', views.profil_recruteur, name='profil_recruteur'),
+    path('offres/', views.mes_offres, name='mes_offres'),
+    path('offres/nouvelle/', views.creer_offre, name='creer_offre'),
+    path('offres/<int:offre_id>/modifier/', views.modifier_offre, name='modifier_offre'),
+    path('offres/<int:offre_id>/supprimer/', views.supprimer_offre, name='supprimer_offre'),
+   path('profil-candidat/', views.profil_candidat, name='profil_candidat'),
+   path('offres-disponibles/', views.offres_dispo, name='offres_dispo'),
+]
